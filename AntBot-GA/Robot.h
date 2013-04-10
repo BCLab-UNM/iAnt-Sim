52d2bd31774c3c082dbaec698e37f199f1ef180b
Joshua Hecker <jhecker@gmail.com>
2013-04-10 14:19:58 -0600

    Robot -Added stepSize variable

--- AntBot-GA/Robot.h
+++ AntBot-GA/Robot.h
@@ -27,6 +27,7 @@
 @property (nonatomic) int searchTime; //Amount of ticks the robot has been performing a random walk.
 @property (nonatomic) int lastMoved; //tick at which the robot last moved (used in random walk).
 @property (nonatomic) int lastTurned; //tick at which the robot last turned (used in random walk).
+@property (nonatomic) int stepSize; //Number of grid cells robot moves before turning
 
 @property (nonatomic) Tag* carrying; //Reference to which Tag the robot is carrying, if any.  nil otherwise.
 @property (nonatomic) int neighbors; //Yeah this sucks, but the problem is we have to know what the neighbor count of the seed was AT THE TIME THE SEED WAS COLLECTED.  We can't just calculate it when the robot returns to the nest because the state of the grid could be mutated, leading to undesired behavior.  Oh well.

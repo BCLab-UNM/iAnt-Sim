8afa66a08c657fa312321affb747867da6ab8afb
Joshua Hecker <jhecker@gmail.com>
2013-04-22 10:34:38 -0600

    Globals -New class to hold previous define values (default values for Simulation)

---
+++ AntBot-GA/Globals.h
@@ -0,0 +1,24 @@
+//
+//  Globals.h
+//  AntBot-GA
+//
+//  Created by Joshua Hecker on 4/21/13.
+//  Copyright (c) 2013 AntBot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Globals : NSObject
+
+extern int pileRadius;
+extern int searchDelay;
+extern int crossoverRate;
+extern int stepCount;
+extern int gridWidth;
+extern int gridHeight;
+extern int gridSize;
+extern int nestX;
+extern int nestY;
+extern int M_2PI;
+
+@end
---
+++ AntBot-GA/Globals.m
@@ -0,0 +1,26 @@
+//
+//  Globals.m
+//  AntBot-GA
+//
+//  Created by Joshua Hecker on 4/21/13.
+//  Copyright (c) 2013 AntBot. All rights reserved.
+//
+
+#import "Globals.h"
+
+@implementation Globals
+
++(void) initialize {
+    pileRadius = 2;
+    searchDelay = 4;
+    crossoverRate = 10;
+    stepCount = 6750;
+    gridWidth = 90;
+    gridHeight = 90;
+    gridSize = gridWidth * gridHeight;
+    nestX = gridWidth / 2;
+    nestY = gridHeight / 2;
+    M_2PI = 2 * M_PI;
+}
+
+@end

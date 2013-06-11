2f11d0431fc149282faeee1453a8f22f0d978b0e
tie372 <tie.372@gmail.com>
2013-06-10 21:31:30 -0600

    Made constants in Constants.h static.

--- AntBot-GA/Constants.h
+++ AntBot-GA/Constants.h
@@ -1,7 +1,7 @@
 #ifndef __ANTBOT_GA_CONSTANTS_H
 #define __ANTBOT_GA_CONSTANTS_H
 
-const int M_2PI = 2 * M_PI;
-const NSPoint NSNullPoint = {-1, -1};
+static const int M_2PI = 2 * M_PI;
+static const NSPoint NSNullPoint = {-1, -1};
 
 #endif

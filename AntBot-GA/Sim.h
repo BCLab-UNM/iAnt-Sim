b3ff97b86646084f2c4612897fc634caa6362cca
Joshua Hecker <jhecker@gmail.com>
2013-06-16 00:37:59 -0600

    Sim -Renamed GA.h to Sim.h to avoid confusion with new GA class

---
+++ AntBot-GA/Sim.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+#import <AntBot-GA/Constants.h>
+#import <AntBot-GA/Util.h>
+#import <AntBot-GA/Array2D.h>
+#import <AntBot-GA/Pheromone.h>
+#import <AntBot-GA/Simulation.h>
+#import <AntBot-GA/Robot.h>
+#import <AntBot-GA/Team.h>
+#import <AntBot-GA/Tag.h>

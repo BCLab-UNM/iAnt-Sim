94266fc9020e69eb54019ae529f0eca0db917bdb
Joshua Hecker <jhecker@gmail.com>
2014-01-13 20:01:29 -0700

    Sim -Changed Util.h to Utilities.h

--- AntBot-GA/Sim.h
+++ AntBot-GA/Sim.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
 #import <AntBot-GA/Constants.h>
-#import <AntBot-GA/Util.h>
+#import <AntBot-GA/Utilities.h>
 #import <AntBot-GA/Array2D.h>
 #import <AntBot-GA/Pheromone.h>
 #import <AntBot-GA/Simulation.h>

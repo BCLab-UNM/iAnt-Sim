c602015ae3ad2bcf738d2a5689f05dab1317631f
Joshua Hecker <jhecker@gmail.com>
2014-01-19 13:32:50 -0700

    GA -Added private variable selectionOperator to support selection setting -Init method now takes selectionOperator argument -Added property fixedVarianceSigma to set variance for fixed variance mutation - default value of 0.05 -Added rankBasedElitistSelection method, emulates selection described in O’Dowd 2011 -Population is now first sorted by increasing fitness - simplifies elitism and rank-based selection

--- AntBot-GA/GA.h
+++ AntBot-GA/GA.h
@@ -1,17 +1,20 @@
 #import <Foundation/Foundation.h>
 #import "Team.h"
-#import "Util.h"
+#import "Utilities.h"
 
 @interface GA : NSObject {
     BOOL elitism;
     float crossoverRate;
     float mutationRate;
+    int selectionOperator;
     int mutationOperator;
     int crossoverOperator;
 }
 
--(id)initWithElitism:(BOOL)_elitism crossoverRate:(float)_crossoverRate crossoverOperator:(int)crossoverOperator mutationRate:(float)_mutationRate andMutationOperator:(int)mutationOperator;
+-(id)initWithElitism:(BOOL)_elitism selectionOperator:(int)_selectionOperator crossoverRate:(float)_crossoverRate crossoverOperator:(int)crossoverOperator mutationRate:(float)_mutationRate andMutationOperator:(int)mutationOperator;
 
 -(void)breedPopulation:(NSMutableArray *)population AtGeneration:(int)generation andMaxGeneration:(int)maxGenerations;
+
+@property (nonatomic) float fixedVarianceSigma;
 
 @end

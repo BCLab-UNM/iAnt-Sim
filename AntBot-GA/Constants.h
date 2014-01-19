ae258469e196d12a7095c6af646ed2f18325fed6
Joshua Hecker <jhecker@gmail.com>
2014-01-19 13:23:55 -0700

    Constants -Added new GA selection const RankBasedElitistSelectionId

--- AntBot-GA/Constants.h
+++ AntBot-GA/Constants.h
@@ -4,6 +4,9 @@
 static const int M_2PI = 2 * M_PI;
 static const NSPoint NSNullPoint = {-1, -1};
 
+static const int TournamentSelectionId = 0;
+static const int RankBasedElitistSelectionId = 1;
+
 static const int IndependentAssortmentCrossId = 0;
 static const int UniformPointCrossId = 1;
 static const int OnePointCrossId = 2;
@@ -12,4 +15,5 @@ static const int TwoPointCross = 3;
 static const int ValueDependentVarMutId = 0;
 static const int DecreasingVarMutId = 1;
 static const int FixedVarMutId = 2;
+
 #endif

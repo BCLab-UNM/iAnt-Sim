934f51ea08069bac4885a89f001dbd1c68b5f21f
Joshua Hecker <jhecker@gmail.com>
2014-01-22 20:43:15 -0700

    Pheromone -Imported Utilities -Moved getPheromone method from Simulation -Added decayRate property - allows each pheromone waypoint to decay at a different rate -Renamed some properties for clarity

--- AntBot-GA/Pheromone.m
+++ AntBot-GA/Pheromone.m
@@ -2,16 +2,47 @@
 
 @implementation Pheromone
 
-@synthesize position;
-@synthesize n,updated;
+@synthesize position, weight, decayRate, updatedTick;
 
--(id) initWithPosition:(NSPoint)_position weight:(float)_n andUpdatedTick:(int)_updated {
+-(id) initWithPosition:(NSPoint)_position weight:(float)_weight decayRate:(float)_decayRate andUpdatedTick:(int)_updatedTick {
     if(self = [super init]) {
         position = _position;
-        n = _n;
-        updated = _updated;
+        weight = _weight;
+        decayRate = _decayRate;
+        updatedTick = _updatedTick;
     }
     return self;
 }
+
+/*
+ * Picks a pheromone out of the passed list based on a random number weighted on the pheromone strengths
+ */
++(NSPoint) getPheromone:(NSMutableArray*)pheromones atTick:(int)tick {
+    float nSum = 0.f;
+    
+    for(int i = 0; i < [pheromones count]; i++) {
+        Pheromone* pheromone = [pheromones objectAtIndex:i];
+        [pheromone setWeight:exponentialDecay([pheromone weight], tick - [pheromone updatedTick], [pheromone decayRate])];
+        if([pheromone weight] < .001) {
+            [pheromones removeObjectAtIndex:i];
+            i--;
+        }
+        else {
+            [pheromone setUpdatedTick:tick];
+            nSum += [pheromone weight];
+        }
+    }
+    
+    float r = randomFloat(nSum);
+    for(Pheromone* pheromone in pheromones) {
+        if(r < [pheromone weight]) {
+            return [pheromone position];
+        }
+        r -= [pheromone weight];
+    }
+    
+    return NSNullPoint;
+}
+
 
 @end

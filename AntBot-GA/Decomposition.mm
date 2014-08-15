4aa4e1b5e3618a5c42725dd9ce12064ab72c8749
Joshua Hecker <jhecker@gmail.com>
2014-08-14 18:27:52 -0600

    Decomposition -Added check for dirty flag; avoids rechecking regions that are unchanged since the previous scan -checkExploredness method now sets the region of all checked cells and marks regions clean once checked -Minor correction of floating point scalars to ints

--- AntBot-GA/Decomposition.mm
+++ AntBot-GA/Decomposition.mm
@@ -26,29 +26,34 @@
     NSMutableArray* allChildren = [[NSMutableArray alloc] init];
     
     for(QuadTree* region in regions) {
-        [region setPercentExplored:[self checkExploredness:region]];
-        if([region percentExplored] == 0 && ([region area] >= 4)) {
-            [unexploredRegions addObject:region];
+        if ([region dirty]) {
+            [region setPercentExplored:[self checkExploredness:region]];
+            if([region percentExplored] == 0 && ([region area] >= 4)) {
+                [unexploredRegions addObject:region];
+            }
+            else if(([region percentExplored] <= .5) && ([region area] >= 4)){
+                [parents addObject:region];
+            }
         }
-        else if(([region needsDecomposition] && [region percentExplored] <= .5) && ([region area] >= 4)){
-            [parents addObject:region];
+        else {
+            [unexploredRegions addObject:region];
         }
     }
     
     for(QuadTree* parent in parents) {
         if(fmod([parent shape].size.width, 2) == 0) {
-            width1 = width2 = [parent shape].size.width / 2.;
+            width1 = width2 = [parent shape].size.width / 2;
         }
         else {
-            width1 = [parent shape].size.width / 2.;
-            width2 = ([parent shape].size.width / 2.) + 1;
+            width1 = [parent shape].size.width / 2;
+            width2 = ([parent shape].size.width / 2) + 1;
         }
         if(fmod([parent shape].size.height, 2) == 0) {
-            height1 = height2 = [parent shape].size.height / 2.;
+            height1 = height2 = [parent shape].size.height / 2;
         }
         else {
-            height1 = [parent shape].size.height / 2.;
-            height2 = ([parent shape].size.height / 2.) + 1;
+            height1 = [parent shape].size.height / 2;
+            height2 = ([parent shape].size.height / 2) + 1;
         }
         
         QuadTree* northWest = [[QuadTree alloc] initWithRect:NSMakeRect([parent shape].origin.x, [parent shape].origin.y, width1, height1)];
@@ -76,20 +81,19 @@
 -(double) checkExploredness:(QuadTree*)region {
     double exploredCount = 0.;
     NSRect shape = [region shape];
+    
     for(int i = shape.origin.y; i < shape.origin.y + shape.size.height; i++) {
         for(int j = shape.origin.x; j < shape.origin.x + shape.size.width; j++) {
+            [grid[i][j] setRegion:region];
             if([grid[i][j] isExplored]) {
                 exploredCount++;
             }
         }
     }
     
-    double newPercent = exploredCount / (double)[region area];
-    if(newPercent > [region percentExplored]) {
-        [region setNeedsDecomposition:YES];
-    }
+    [region setDirty:NO];
     
-    return newPercent;
+    return exploredCount / [region area];
 }
 
 @end

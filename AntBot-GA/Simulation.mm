43ac053f71ee695ac924c022cdf0a46e1f4790ac
Karl Stolleis <karl.stolleis@gmail.com>
2014-11-17 17:02:08 -0700

    test run with obstacle count fixed - thanks Bjorn

--- AntBot-GA/Simulation.mm
+++ AntBot-GA/Simulation.mm
@@ -29,11 +29,11 @@ using namespace cv;
 -(id) init {
     if(self = [super init]) {
         
-        teamCount = 1;                // number of "individuals"
-        generationCount = 10;           // generations show convergence around 20-30 so shrinking from 100
+        teamCount = 100;                // number of "individuals"
+        generationCount = 50;           // generations show convergence around 20-30 so shrinking to 50 from 100
         robotCount = 6;                 // lets leave this at 6 for now
         tagCount = 256;                 // hold steady
-        evaluationCount = 1;           // more for Maricopa
+        evaluationCount = 12;           // more for Maricopa
         evaluationLimit = -1;
         tickCount = 7200;               // 1 hour (two ticks per second)
         clusteringTagCutoff = -1;

524a2452f537dd47075aba3243b36d8cb4f7dc1c
Joshua Hecker <jhecker@gmail.com>
2015-02-08 09:46:04 -0700

    Utilities -Added functions to calculate BIC (Bayesian Information Criteria) and the number of free parameters for an EM (Expectation-Maximization) model

--- AntBot-GA/Utilities.h
+++ AntBot-GA/Utilities.h
@@ -144,3 +144,19 @@ static inline float exponentialCDF(float x, float lambda) {
 static inline float exponentialDecay(float quantity, float time, float lambda) {
     return (quantity * exp(-lambda * time));
 }
+
+/*
+ * Calculates Bayesian Information Criteria (BIC) given log likelihood ll,
+ * free parameters k, and number of data points n
+ */
+static inline float bic(float ll, int k, int n) {
+    return (log(n) * k) - (2 * ll);
+}
+
+/*
+ * Calculates number of free parameters for EM given clusters k and
+ * dimensionality of data d
+ */
+static inline int componentsEM(int k, int d) {
+    return (k - 1) + (k * d) + (k * ((d * (d - 1)) / 2));
+}

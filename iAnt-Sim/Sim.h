1c3392e24312d5e6c05da3bf54e5b448f47c33ba
Joshua Hecker <jhecker@gmail.com>
2015-08-18 10:11:55 -0600

    Project-wide rename to iAnt-Sim

---
+++ iAnt-Sim/Sim.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+#import <iAnt-Sim/Constants.h>
+#import <iAnt-Sim/Utilities.h>
+#import <iAnt-Sim/Pheromone.h>
+#import <iAnt-Sim/Simulation.h>
+#import <iAnt-Sim/Robot.h>
+#import <iAnt-Sim/Team.h>
+#import <iAnt-Sim/Tag.h>
+#import <iAnt-Sim/QuadTree.h>
+#import <iAnt-Sim/Cluster.h>
